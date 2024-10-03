ef082c8ff50029df899d1efcebb0b4d79934380c
ideftbuild <a.akanSWE@gamil.com>
2024-10-04 00:27:48 +0100

    add: implement `Airport` class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  toString() {
+    return `[Object ${this.code}]`;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this.code}`;
+  }
+}
