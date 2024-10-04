a98471cfa575b1399ee8b762d0cbd9e5b6d8d07b
ideftbuild <a.akanSWE@gamil.com>
2024-10-04 01:06:54 +0100

    add: Implement `EVCar` class

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  cloneCar() {
+    return new Car();
+  }
+  /* eslint-enable class-methods-use-this */
+}
