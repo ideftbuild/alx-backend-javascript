1fd71b3cc6eff8b25b92996ef209fa813125651a
ideftbuild <a.akanSWE@gamil.com>
2024-10-10 03:23:29 +0100

    feat: Implement `StudentClass` and interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,7 +4,7 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [propName: string]: any;
+  [propName: string]: boolean | string | number;
 }
 
 interface Directors extends Teacher {
@@ -16,3 +16,36 @@ interface printTeacherFunction {
 }
 
 const printTeacher: printTeacherFunction = (firstName, lastName) => `${firstName.charAt(0)}. ${lastName}`;
+// usage example
+console.log(printTeacher('John', 'Doe'));
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): Student;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework = (): string => 'Currently working';
+
+  displayName =(): string => this.firstName;
+}
+
+// usage example
+const student = new StudentClass('John', 'Doe');
+console.log(student.workOnHomework());
+console.log(student.displayName());
