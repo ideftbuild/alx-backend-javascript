905b5cab60d0565a6af5345ef06f087731b5dd46
ideftbuild <a.akanSWE@gamil.com>
2024-10-10 04:20:11 +0100

    feat: Implement `isDirector` and `executeWork` function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -11,21 +11,38 @@ interface TeacherInterface {
 }
 
 class Director implements DirectorInterface {
-  workFromHome(): string { return 'Working from home'; }
-  getCoffeeBreak(): string { return 'Getting a coffee break'; }
-  workDirectorTasks(): string { return 'Getting to director tasks';}
+  workFromHome = (): string => 'Working from home';
+
+  getCoffeeBreak = (): string => 'Getting a coffee break';
+
+  workDirectorTasks = (): string => 'Getting to director tasks';
 }
 
 class Teacher implements TeacherInterface {
-  workFromHome(): string { return 'Cannot work from home'; }
-  getCoffeeBreak(): string { return 'Cannot have a break'; }
-  workTeacherTasks(): string { return 'Getting to work'; }
+  workFromHome = (): string => 'Cannot work from home';
+
+  getCoffeeBreak = (): string => 'Cannot have a break';
+
+  workTeacherTasks = (): string => 'Getting to work';
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
-  } else {
-    return new Director();
   }
+  return new Director();
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+// usage
+console.log(executeWork(createEmployee(600)));
