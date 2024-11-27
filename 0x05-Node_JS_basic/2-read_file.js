9891d73e8ad262b457492733c16ca4d8a0633f47
ideftbuild <a.akanSWE@gamil.com>
2024-11-27 08:30:56 +0100

    feat: Implement `countStudents` function

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  let data = null;
+  const result = {};
+  try {
+    data = fs.readFileSync(path, 'utf8').trim().split('\n').slice(1);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+  // Process each line
+  data.forEach((line) => {
+    const fields = line.trim().split(',');
+    const [name, , , field] = fields;
+    if (!result[field]) {
+      result[field] = [];
+    }
+    result[field].push(name);
+  });
+
+  // Output result
+  console.log('Number of students:', data.length);
+  Object.entries(result).forEach(([field, list]) => {
+    console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+  });
+};
