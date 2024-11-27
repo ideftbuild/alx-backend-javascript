0797cd29785eee739e81cef56024fb08168bbf36
ideftbuild <a.akanSWE@gamil.com>
2024-11-27 18:04:05 +0100

    feat: add `type` parameter to perform SUM, SUBTRACT, or DIVIDE operations

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function calculateNumber(type, a, b) {
+  a = Math.round(a)
+  b = Math.round(b)
+  if (type === 'SUM') {
+    return a + b;
+  } else if (type === 'SUBTRACT') {
+    return a - b;
+  } else if (type == 'DIVIDE') {
+    return b != 0 ? a / b : 'Error';
+  }
+}
+
+module.exports = calculateNumber
