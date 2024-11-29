f70a80f0620356d26c01fef6c6b7400e73e4f121
ideftbuild <a.akanSWE@gamil.com>
2024-11-29 16:57:33 +0100

    feat: export app instance

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(1245);
+app.listen(1245);
+
+module.exports = app;
