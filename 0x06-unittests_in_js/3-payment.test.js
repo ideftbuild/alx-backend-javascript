bbef4e86fe7f5b2e2c746fcdfcc3ed66f17bc502
ideftbuild <a.akanSWE@gamil.com>
2024-11-29 12:50:23 +0100

    test: Add test suites for `sendPaymentRequestToApi` function

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils.js');
+const sinon = require('sinon');
+
+describe('sendPaymentRequestToApi', function () {
+  const totalAmount = 100;
+  const totalShipping = 20;
+
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('Verify Utils.calculateNumber is called once', function () {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(spy);
+  });
+
+  it('Verify Utils.calculateNumber is called with the same argument',
+    function () {
+      const spy = sinon.spy(Utils, 'calculateNumber');
+      sendPaymentRequestToApi(totalAmount, totalShipping);
+      sinon.assert.calledWith(spy, 'SUM', totalAmount, totalShipping);
+  });
+
+  it('Verify the console.log output', function () {
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+    sinon.assert.calledWith(spy, 'The total is:', 120);
+    spy.restore();
+  });
+});
