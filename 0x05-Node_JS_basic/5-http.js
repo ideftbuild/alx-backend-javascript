4af6f831dbd937524562b07db400c248a6aeb534
ideftbuild <a.akanSWE@gamil.com>
2024-11-30 05:32:21 +0100

    feat: Add handling for missing database argument

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,37 +2,46 @@
 const http = require('http');
 const fs = require('fs');
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   const { url } = req;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
   if (url === '/') {
-    res.end('Hello Holberton School!');
+    res.end('Hello Holberton School!'); // response body
   } else if (url === '/students') {
-    // result = await countStudents(process.argv[2]);
-    let output = 'This is the list of our students\n';
     const result = {};
-    fs.readFile(process.argv[2], 'utf8', (error, data) => {
-      if (error) {
-        throw new Error('Cannot load the database');
-      }
-      const dataList = data.trim().split('\n').slice(1);
-      // Process each line
-      dataList.forEach((line) => {
-        const fields = line.trim().split(',');
-        const [firstName, , , field] = fields;
-        if (!result[field]) {
-          result[field] = [];
+    let output = 'This is the list of our students';
+    const db = process.argv[2];
+    // get data from database if passed
+    if (!db) {
+      res.statusCode = 500;
+      res.end('Cannot load the database');
+    } else {
+      fs.readFile(db, 'utf8', (error, data) => {
+        if (error) {
+          res.statusCode = 500;
+          res.end('Cannot load the database');
+          return;
         }
-        result[field].push(firstName);
+        const dataList = data.trim().split('\n').slice(1);
+        // Process each line
+        dataList.forEach((line) => {
+          const fields = line.trim().split(',');
+          const [firstName, , , field] = fields;
+          if (!result[field]) {
+            result[field] = [];
+          }
+          result[field].push(firstName);
+        });
+        // Store result
+        output += `\nNumber of students: ${dataList.length}`;
+        Object.entries(result).forEach(([field, list]) => {
+          output += `\nNumber of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
+        });
+        res.end(output); // response body
       });
-      // Output result
-      output += `Number of students: ${dataList.length}\n`;
-      Object.entries(result).forEach(([field, list]) => {
-        output += `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}\n`;
-      });
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(output.trim('\n'));
-    });
+    }
   }
 });
 
