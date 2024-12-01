98b8558113dbb8f08b1c2ca9aba46a3aca94f8b8
ideftbuild <a.akanSWE@gamil.com>
2024-12-01 21:01:05 +0100

    fix: remove trailing newline from response string in GET route

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(1245);
