398f6f1b6413549b41cfa3b4abe528bdc46cd58c
ideftbuild <a.akanSWE@gamil.com>
2025-02-06 10:21:40 +0100

    Implement /cart/:id route and test cases

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,38 @@
+const request = require('request');
+const { expect } = require('chai');
+
+const url = 'http://localhost:7865';
+
+describe('Index page', () => {
+
+	it('Accessing / route should succeed without errors', (done) => {
+		request(url, (error, response, body) => {
+			expect(error).to.be.null;
+			done();
+		});
+	});
+
+	it('Verify the response body of the / route', (done) => {
+		request(url, (error, response, body) => {
+			expect(error).to.be.null;
+			expect(body).to.equal('Welcome to the payment system');
+			done();
+		});
+	});
+
+	it('Correct status code when :id is a number', (done) => {
+		request(url + '/cart/12', (error, response, body) => {
+			expect(error).to.be.null;
+			expect(body).to.equal('Payment methods for cart 12');
+			done();
+		});
+	})
+
+	it('Correct status code when :id is not a number (=> 404)', (done) => {
+		request(url + '/cart/ae', (error, response, body) => {
+			expect(error).to.be.null;
+			expect(response.statusCode).to.be.at.least(404);
+			done();
+		});
+	})
+})
