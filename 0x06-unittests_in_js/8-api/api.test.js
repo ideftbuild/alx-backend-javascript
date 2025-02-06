58cbbddb746923039b7979ea3d71c91c423123e0
ideftbuild <a.akanSWE@gamil.com>
2025-02-06 09:50:05 +0100

    Ensure Mocha waits for the async request to finish

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,16 +5,18 @@ const url = 'http://localhost:7865';
 
 describe('Index page', () => {
 
-	it('Accessing / route should succeed without errors', async () => {
+	it('Accessing / route should succeed without errors', (done) => {
 		request(url, (error, response, body) => {
 			expect(error).to.be.null;
+			done();
 		});
 	});
 
-	it('Verify the response body of the / route', () => {
+	it('Verify the response body of the / route', (done) => {
 		request(url, (error, response, body) => {
 			expect(error).to.be.null;
 			expect(body).to.equal('Welcome to the payment system');
+			done();
 		});
 	});
 })
